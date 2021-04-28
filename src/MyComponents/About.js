55b0be4c012d373facec675fb1ef339826c0ab3d
Priyanshu Mishra <63869238+priyanshu0171@users.noreply.github.com>
2021-04-28 20:54:09 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur iusto eligendi iure incidunt ad mollitia quos, enim aliquam maxime laborum sint molestiae perspiciatis odio, impedit, deleniti laboriosam est rerum dolor a dolorum nihil. Voluptate.</p>
+        </div>
+    )
+}
